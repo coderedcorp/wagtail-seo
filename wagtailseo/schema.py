2efdc45d3a7793ba458c678cc08f3a0f13b17a91
Martin Anev <martianev@gmail.com>
2021-01-26 10:37:12 +0100

    - Refactoring

--- wagtailseo/schema.py
+++ wagtailseo/schema.py
@@ -153,10 +153,10 @@ SCHEMA_ORG_CHOICES = (
     ("TheaterGroup", "Organization > PerformingGroup > TheaterGroup"),
     ("SportsOrganization", "Organization > SportsOrganization"),
     ("SportsTeam", "Organization > SportsOrganization > SportsTeam"),
-    ("JobPosting","Thing > Intangible > JobPosting"),
     ("Article", "Thing > CreativeWork > Article"),
     ("NewsArticle", " Thing > CreativeWork > Article > NewsArticle "),
     ("VideoObject", " Thing > CreativeWork > MediaObject > VideoObject "),
+    ("JobPosting", "Thing > Intangible > JobPosting"),
 )
 
 SCHEMA_ACTION_CHOICES = (
