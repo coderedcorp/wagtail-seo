7f19bb09618e33ba516b622829d7e94b8d432e83
Roxanna Coldiron <roxanna@coderedcorp.com>
2021-08-03 14:54:44 -0400

    reformatted

--- wagtailseo/apps.py
+++ wagtailseo/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class WagtailSeoConfig(AppConfig):
     name = "wagtailseo"
     verbose_name = "Wagtail SEO"
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = "django.db.models.AutoField"
