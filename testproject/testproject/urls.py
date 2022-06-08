c289d425174c91ce03e84bfe1c113b0ef7d3dd43
Vince Salvino <salvino@coderedcorp.com>
2022-06-08 12:24:21 -0400

    Fix for Django 4.0

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, path
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -8,13 +8,13 @@ from wagtail.documents import urls as wagtaildocs_urls
 
 
 urlpatterns = [
-    url(r"^django-admin/", admin.site.urls),
-    url(r"^admin/", include(wagtailadmin_urls)),
-    url(r"^documents/", include(wagtaildocs_urls)),
+    path("django-admin/", admin.site.urls),
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    url(r"", include(wagtail_urls)),
+    path("", include(wagtail_urls)),
 ]
 
 
